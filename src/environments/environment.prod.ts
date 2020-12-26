589287604495d74bc236e79fea512085521fe982
Catalin <icatalin201@gmail.com>
2020-12-26 15:42:53 +0200

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.100.30:8888/api/v1',
-  keycloakUrl: 'http://192.168.100.30:8080',
+  apiUrl: 'http://192.168.100.35:9999/api/v1',
+  keycloakUrl: 'http://192.168.100.35:8080',
   contentType: 'application/vnd.plutus.finance+json'
 };
